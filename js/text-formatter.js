1061232f12356fe08070a1e5b3a4550921b06592
lihaitao <lihtz@digiwin.com>
2021-12-08 14:04:47 +0800

    修改代码编辑器

--- js/text-formatter.js
+++ js/text-formatter.js
@@ -1,50 +1,72 @@
 $(function() {
+
+	var sourceTextArea = CodeMirror.fromTextArea($('#source')[0], {
+	  lineNumbers: true,
+	  tabSize: 4
+	});
+
+	var targetTextArea = CodeMirror.fromTextArea($('#target')[0], {
+	  lineNumbers: true,
+	  tabSize: 4
+	});
+
+
+	// 格式化
 	$('#format-btn').on('click', function() {
-		let sourceText = $('#source').val();
+		let sourceText = sourceTextArea.getValue();
 		try {
 			let sourceObj = JSON.parse(sourceText);
 			let targetText = JSON.stringify(sourceObj, null, 4);
-			$('#target').val(targetText);
+			targetTextArea.setValue(targetText);
 		} catch (err) {
 			console.error(err);
-			$('#target').val("JSON格式不正确");
+			targetTextArea.setValue("JSON格式不正确");
 		}
 	});
 
+	// 压缩
 	$('#compress-btn').on('click', function() {
-		let sourceText = $('#source').val();
+		let sourceText = sourceTextArea.getValue();
 		try {
 			let sourceObj = JSON.parse(sourceText);
 			let targetText = JSON.stringify(sourceObj, null, 0);
-			$('#target').val(targetText);
+			targetTextArea.setValue(targetText);
 		} catch (err) {
 			console.error(err);
-			$('#target').val("JSON格式不正确");
+			targetTextArea.setValue("JSON格式不正确");
 		}
 	});
 
+	// 压缩转义
 	$('#compress-and-escape-btn').on('click', function() {
-		let sourceText = $('#source').val();
+		let sourceText = sourceTextArea.getValue();
 		try {
 			let sourceObj = JSON.parse(sourceText);
 			let targetText = JSON.stringify(sourceObj, null, 0);
 			targetText = JSON.stringify(targetText);
-			$('#target').val(targetText);
+			targetTextArea.setValue(targetText);
 		} catch (err) {
 			console.error(err);
-			$('#target').val("JSON格式不正确");
+			targetTextArea.setValue("JSON格式不正确");
 		}
 	});
 
+	// 去除转义
 	$('#remove-escape-btn').on('click', function() {
-		let sourceText = $('#source').val();
+		let sourceText = sourceTextArea.getValue();
 		try {
 			let sourceObj = JSON.parse(sourceText);
-			$('#target').val(sourceObj);
+			targetTextArea.setValue(sourceObj);
 		} catch (err) {
 			console.error(err);
-			$('#target').val("JSON格式不正确");
+			targetTextArea.setValue("JSON格式不正确");
 		}
+	});
+
+	// 切换选项卡
+	$('.tab').on('click', function() {
+		$('.tab').removeClass('active');
+		$(this).addClass('active');
 	});
 
 });
